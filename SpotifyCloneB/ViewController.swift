eac72cb5f92f784afd7c26cd28fd10894e6b9e26
linder3hs <linderhassinger00@gmail.com>
2022-05-07 08:33:32 -0500

    Initial Commit

---
+++ SpotifyCloneB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyCloneB
+//
+//  Created by Linder Anderson Hassinger Solano    on 7/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
